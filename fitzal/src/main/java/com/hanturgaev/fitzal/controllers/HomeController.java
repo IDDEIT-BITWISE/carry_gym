d4e9d56d5b463346b754c45e9546a9d195f8ae07
frayzkilla <frayzthamotion@gmail.com>
2025-02-19 19:24:01 +0800

    add home controller

---
+++ fitzal/src/main/java/com/hanturgaev/fitzal/controllers/HomeController.java
@@ -0,0 +1,75 @@
+package com.hanturgaev.fitzal.controllers;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+
+    @GetMapping("/")
+    public String index(){
+
+        return "home";
+    }
+
+
+    @GetMapping(value = "/login")
+    public String goAuth() {
+        return "login";
+    }
+
+    @GetMapping(value = "/badPage")
+    public String goBad() {
+        return "badPage";
+    }
+
+    @GetMapping(value = "/news")
+    public String goNews() {
+        return "news";
+    }
+
+    @GetMapping(value = "/contacts")
+    public String goContacts() {
+        return "contacts";
+    }
+
+
+    @GetMapping(value = "/register")
+    public String goRegister() {
+        return "register";
+    }
+
+//    @PostMapping(value = "/register")
+//    public String register(@ModelAttribute User user, HttpServletRequest request,
+//                           @CurrentSecurityContext SecurityContext securityContext) {
+//        userService.save(user);
+//
+//        //Program authentication
+//        Authentication authentication = new UsernamePasswordAuthenticationToken(
+//                user, user.getPassword(), user.getAuthorities());
+//
+//        securityContext.setAuthentication(authentication);
+//
+//        // Create a new session and add the security context.
+//        HttpSession session = request.getSession(true);
+//        session.setAttribute("SPRING_SECURITY_CONTEXT", securityContext);
+//        return "redirect:/";
+//    }
+
+
+}
